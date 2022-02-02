cdd757dd1947386d11a2e6ef34855250a2f3a8b5
craftina <hristina.rum.hristova@gmail.com>
2022-02-02 13:08:27 +0200

    creating react app and basic html elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="react-quiz-container">
+        <div class="question-wrapper">
+          <div class="question">
+            <h3>This is a question</h3>
+          </div>
+          <div class="answers">
+            <button class="answer-btn">a) Answer 1</button>
+            <button class="answer-btn">b) Answer 2</button>
+            <button class="answer-btn">c) Answer 3</button>
+            <button class="answer-btn">d) Answer 4</button>
+          </div>
+        </div>
+
+      </div>
+      {/* <img src="../puplic/images/background_image.jpg" /> */}
+
+    </div> 
   );
 }
 
