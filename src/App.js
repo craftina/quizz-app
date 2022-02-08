5336ecb5aa14493414eef749ce05bd9151ee86d0
craftina <hristina.rum.hristova@gmail.com>
2022-02-08 12:26:17 +0200

    adding result btn and using state for saving answers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 import './App.css';
 import myData from './data/questions.json';
 import Question from './components/Questions/Questions';
-import {useState, useContext} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   let [current, setCurrent] = useState(1);
   let [currentQuestion, setCurrentQuestion] = useState(myData.questions["1"]);
-  
-  function onClickNext(){
+  let [answers, setAnswers] = useState([]);
+  let [submitButton, setSubmitButton] = useState('Next');
+
+
+  function onClickNext(ev) {
+    ev.preventDefault();
+
+    const form = ev.target.parentNode.parentNode;
+    const buttons = form.querySelectorAll('input');
+    console.log(buttons);
+    for (let i = 0; i < buttons.length; i++) {
+      const el = buttons[i];
+      if (el.classList.contains("active")) {
+
+        setAnswers(oldState => (
+          [...oldState, el.name]));
+
+      } else {
+        console.log(false);
+      }
+    }
+
     setCurrent(++current);
     setCurrentQuestion(myData.questions[`${current}`]);
   }
 
-  
+
+  function onClickResult() {
+    console.log('result');
+  }
+
+  useEffect(() => {
+    setSubmitButton(() =>
+      Object.keys(myData.questions).length === (answers.length + 1)
+        ? 'Result'
+        : 'Next'
+    );
+  }, [answers.length])
+  // console.log(Object.keys(myData.questions).length);
+  // console.log(answers.length);
+  // console.log(Object.keys(myData.questions).length);
+
+
 
   return (
     <div className="App">
       <div className="react-quiz-container">
         <div className="question-wrapper">
-          <Question data={currentQuestion} />
-          <div className="next">
-            <button onClick={onClickNext}>Next</button>
-          </div>
+          <form>
+
+            <Question data={currentQuestion} />
+            <div className="next-btn">
+              {submitButton === 'Result'
+                ? <button onClick={onClickResult}>Result</button>
+                : <button onClick={onClickNext}>Next</button>
+              }
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -4,7 +4,7 @@ import './Questions.css';
 const Question = (props) => {
     const data = props.data;
 
-    function onClickButton(ev){
+    function onClickButton(ev) {
         const button = ev.target;
         const allButtons = ev.target.parentElement.children;
 
@@ -13,24 +13,26 @@ const Question = (props) => {
             btn.classList.remove("active");
         }
         
-        if(button.classList.contains("active")){
-            button.classList.remove("active")
-        } else{
-            button.classList.add("active")
+        if (button.classList.contains("active")) {
+            button.classList.remove("active");
+        } else {
+            button.classList.add("active");
         }
     }
 
     return (
-            <div>
-                <div className="question">
-                    <h3>{data.question}</h3>
-                </div>
-                <div className="answers">
-                    {data.answers.map(a => {
-                        return  <button key={`${data.id}${a.bullet}`} onClick={onClickButton} className="answer-btn">{a.bullet}) {a.answerText}</button>
-                    })}
-                </div>
+        <div>
+            <div className="question">
+                <h3>{data.question}</h3>
             </div>
+            <div className="answers">
+                {data.answers.map(a => {
+                    return (
+                        <input key={`${data.id}${a.bullet}`} type="button"  className="answer-btn" 
+                        name={a.answerText} onClick={onClickButton} value={`${a.bullet}) ${a.answerText}`}/>                    
+                )})}
+            </div>
+        </div>
     );
 }
 export default Question;
